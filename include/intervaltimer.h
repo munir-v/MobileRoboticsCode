295fd1eeb32f18c56b20968d498bc8e85f9e4790
anthonyjclark <anthonyjclark@gmail.com>
2024-10-16 15:47:37 -0700

    Added a `getLastDelta` method to the interval timer class.

--- include/intervaltimer.h
+++ include/intervaltimer.h
@@ -5,16 +5,21 @@ class IntervalTimer {
  private:
   unsigned long interval;
   unsigned long lastTime;
+  unsigned long delta;
 
  public:
-  IntervalTimer(unsigned long interval) : interval(interval), lastTime(0) {}
+  IntervalTimer(unsigned long interval) : interval(interval), lastTime(0), delta(0) {}
 
   void setLastTime(unsigned long lastTime) { this->lastTime = lastTime; }
 
+  unsigned long getLastDelta() { return delta; }
+
   // The boolean operator contains the most pertinent logic for the IntervalTimer class.
+  // It is "called" whenever the timer is used in a conditional expression.
   operator bool() {
     unsigned long now = millis();
-    if (now - lastTime >= interval) {
+    delta = now - lastTime;
+    if (delta >= interval) {
       lastTime = now;
       return true;
     }
