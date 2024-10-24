adcfea783f2abba130379aa80db9508ffc765313
munir-v <81745551+munir-v@users.noreply.github.com>
2024-10-24 10:53:01 -0700

    updates

--- Examples/ForwardKinematics/ForwardKinematics.ino
+++ Examples/ForwardKinematics/ForwardKinematics.ino
@@ -4,7 +4,7 @@
 #include "../../include/display.h"
 
 // Network configuration
-const char* SSID = "Pomona";
+const char *SSID = "Pomona";
 const uint16_t PORT = 8181;
 const unsigned long HEARTBEAT_INTERVAL = 1000;
 
@@ -12,14 +12,17 @@ const unsigned long HEARTBEAT_INTERVAL = 1000;
 WsCommunicator wsCommunicator(SSID, PORT, HEARTBEAT_INTERVAL);
 Display display;
 MotorControl motorController(0.2, .5, .5, .5, .5, 0.34, 100);
-DifferentialDriveRobot diffDriveRobot(0.2);  // Wheelbase of 0.2 meters
+DifferentialDriveRobot diffDriveRobot(0.2); // Wheelbase of 0.2 meters
+
 unsigned long totalTime = 0;
 unsigned long delta_t = 0.1;
+unsigned long lastMotorUpdateTime = 0;
 
 //
 // Setup function
 //
-void setup() {
+void setup()
+{
   // Start serial communication
   Serial.begin(115200);
 
@@ -30,7 +33,7 @@ void setup() {
   motorController.setup();
 
   // Set initial motor target velocity
-  motorController.setTargetVelocity(0.1);  // 0.1 m/s
+  motorController.setTargetVelocity(0.1); // 0.1 m/s
 
   // Initialize display
   display.setup();
@@ -39,8 +42,7 @@ void setup() {
   display.drawString(0, 0, "IP: " + WiFi.localIP().toString());
 
   // Initialize robot's position and orientation
-  diffDriveRobot.setup();  // Reset x, y, and theta to zero
-
+  diffDriveRobot.setup(); // Reset x, y, and theta to zero
 
   totalTime = 0;
 }
@@ -48,32 +50,41 @@ void setup() {
 //
 // Loop function
 //
-void loop() {
+void loop()
+{
   // Process WebSocket communication
   wsCommunicator.loopStep();
+  motorController.loopStep(true);
 
   // Check if the WebSocket communicator is enabled
-  if (wsCommunicator.isEnabled()) {
-    Serial.print("WebSocket is enabled\n");
-    display.drawString(1, 0, "TEST");
-
+  if (wsCommunicator.isEnabled())
+  {
+    // Check if the total time is greater than 10 seconds
+    if (totalTime > 10)
+    {
+      motorController.stop();
+      return;
+    }
 
     // Set left and right wheel velocities
-    motorController.setTargetVelocity(1,2);
+    motorController.setTargetVelocity(1, 2);
 
     // Get wheel velocities from the motor encoder
     double v_L = motorController.getLeftVelocity();  // Left wheel velocity
     double v_R = motorController.getRightVelocity(); // Right wheel velocity
+    
+    unsigned long now = millis();
 
     // Update robot's kinematics using wheel velocities and time step
-    while (totalTime <= 10 ) {
-      motorController.loopStep(true);
+    if (now - lastMotorUpdateTime > delta_t)
+    {
+      lastMotorUpdateTime = now;
       diffDriveRobot.forward_kinematics(v_L, v_R, delta_t);
-      v_L = motorController.getLeftVelocity();  // Left wheel velocity
-      v_R = motorController.getRightVelocity(); // Right wheel velocity
       totalTime += delta_t;
     }
-  } else {
+  }
+  else
+  {
     // Stop the motor if WebSocket is disabled
     motorController.stop();
   }
