c2939e15aef1d0ee076720cb381900cd4c3a0112
Russel Otieno <russel.docs@gmail.com>
2024-11-06 20:33:52 -0800

    slight modifications with insight from lecture video on position control

--- Examples/PositionControl/PositionControl.ino
+++ Examples/PositionControl/PositionControl.ino
@@ -4,9 +4,19 @@
 #include "../ForwardKinematics/kinematics.h"
 #include "../../include/display.h"
 
+
+// physical configs
+const float WHEEL_DIAMETER = 0.07;
+const float WHEEL_CIRCUMFERENCE =  PI * WHEEL_DIAMETER;
+const float WHEEL_BASE = 0.2;
+
+
 // Network configuration
 const char *SSID = "Pomona";
 const uint16_t PORT = 8181;
+
+
+// position control configs
 const unsigned long HEARTBEAT_INTERVAL = 1000;
 const double GOALX = 1;
 const double GOALY = 1;
@@ -17,16 +27,31 @@ const double MAX_ANGULAR_VELOCITY = 0;
 const double K_POSITION = 0;
 const double K_ORIENTATION = 0;
 
-// TODO THIS MIGHT NEED TO BE CHANGED
-double v_L, v_R = 0;
+
+
+
+// forward kinematics interval
+const float FORWARD_KINEMATIC_INTERVAL = 100;
+
+// // TODO THIS MIGHT NEED TO BE CHANGED
+// double v_L, v_R = 0;
+
 
 // Instances of classes
 WsCommunicator wsCommunicator(SSID, PORT, HEARTBEAT_INTERVAL);
 MotorControl motorController(0.2, .5, .5, .5, .5, 0.34, 250);
 Display display;
+
+
+
+
+const unsigned long POSITION_CONTROL_INTERVAL:
 PositionControl positionControl(
-    GOALX, GOALY, GOAL_THRESHOLD, TRACK_WIDTH, MAX_LINEAR_VELOCITY, MAX_ANGULAR_VELOCITY, K_POSITION, K_ORIENTATION);
-DifferentialDriveRobot diffDriveRobot(0.2); // Wheelbase of 0.2 meters
+   GOALX, GOALY, GOAL_THRESHOLD, TRACK_WIDTH, MAX_LINEAR_VELOCITY, MAX_ANGULAR_VELOCITY, K_POSITION, K_ORIENTATION, TRACK_WIDTH, POSITION_CONTROL_INTERVAL);
+
+
+DifferentialDriveRobot diffDriveRobot(WHEEL_BASE); // Wheelbase of 0.2 meters
+
 
 double totalTime = 0;
 double delta_t = 0.25;
@@ -34,73 +59,76 @@ double lastMotorUpdateTime = 0;
 Pose curr_pose;
 Pose goal_pose;
 
+
 //
 // Setup function
 //
 void setup()
 {
-    // Start serial communication
-    Serial.begin(115200);
+   // Start serial communication
+   Serial.begin(115200);
 
-    // Initialize the network communicator
-    wsCommunicator.setup();
 
-    // Initialize motor control
-    motorController.setup();
+   // Initialize the network communicator
+   wsCommunicator.setup();
 
-    // Set initial motor target velocity
-    motorController.setTargetVelocity(0.1); // 0.1 m/s
 
-    // Initialize display
-    display.setup();
+   // Initialize motor control
+   motorController.setup();
 
-    // Display IP address on the OLED screen
-    display.drawString(0, 0, "IP: " + WiFi.localIP().toString());
 
-    // Initialize robot's position and orientation
-    diffDriveRobot.setup(); // Reset x, y, and theta to zero
+   // Set initial motor target velocity
+   // motorController.setTargetVelocity(0.1); // 0.1 m/s
 
-    totalTime = 0;
+
+   // Initialize display
+   display.setup();
+
+
+   // Display IP address on the OLED screen
+   display.drawString(0, 0, "IP: " + WiFi.localIP().toString());
+   display.drawString(1, 0, "Position Control");
+
+
+   // Initialize robot's position and orientation
+   diffDriveRobot.setup(); // Reset x, y, and theta to zero
+   positionControl.setup()
+
+
+   totalTime = 0;
 }
-
 //
 // Loop function
 //
 void loop()
 {
-    // Process WebSocket communication
-    wsCommunicator.loopStep();
-    motorController.loopStep(true);
+   // Process WebSocket communication
+   wsCommunicator.loopStep();
 
-    // Check if the WebSocket communicator is enabled
-    if (wsCommunicator.isEnabled())
-    {
-        // Check if the total time is greater than 10 seconds
-        if (totalTime > 10)
-        {
-            motorController.stop();
-            return;
-        }
-        // Set left and right wheel velocities
-        motorController.setTargetVelocity(v_L, v_R);
+   if (wsCommunicator.resetFlagIsSet()) {
+       diffDriveRobot.reset();
+       wsCommunicator.clearResetFlag();
+   }
 
-        unsigned long now = millis();
+   display.loopStep()
 
-        // Update robot's kinematics using wheel velocities and time step
-        if (now - lastMotorUpdateTime > (delta_t * 1000)) // Convert seconds to milliseconds
-        {
-            lastMotorUpdateTime = now;
-            diffDriveRobot.forward_kinematics(v_L, v_R, delta_t);
-            curr_pose.x = diffDriveRobot.get_x();
-            curr_pose.y = diffDriveRobot.get_y();
-            curr_pose.theta = diffDriveRobot.get_theta();
-            v_L, v_R = positionControl.position_control(curr_pose, goal_pose);
-            totalTime += delta_t;
-        }
-    }
-    else
-    {
-        // Stop the motor if WebSocket is disabled
-        motorController.stop();
-    }
-}
+   motorController.loopStep(wsCommunicator.isEnabled());
+
+   float leftVelocity = motorControl.getLeftVelocity();
+   float rightVelocity = motorControl.getRightVelocity();
+
+   diffDriveRobot.forward_kinematics(leftVelocity, rightVelocity, FORWARD_KINEMATIC_INTERVAL);
+
+
+   Pose pose = diffDriveRobot.getPose();
+   bool updateVelocities = positionControl.loopStep(pose, leftVelocity, rightVelocity);
+
+
+   if (updateVelocities) {
+       motorControl.setTagetVelocity(leftVelocity, rightVelocity);
+   }
+
+   if (messageTimer) {
+       snprintf(message, sizeof(message), "x=%f y =%f theta=%f vl=%f vr=%f", pose.x, pose.y, pose.theta, leftVelocity, rightVelocity);
+      wsCommunicator.sendText(message, strlen(message))
+   }
--- Examples/PositionControl/positioncontrol.h
+++ Examples/PositionControl/positioncontrol.h
@@ -4,84 +4,94 @@
 #include "../../include/forwardkinematics.h"
 #include <math.h>
 #include <algorithm>
+#include <utility>
+
 
 class PositionControl
 {
-    // Private variables to store global state
+   // Private variables to store global state
 private:
 
 
 public:
-    // Constructor initializes robot state
-    double goalX;
-    double goalY;
-    double goalThreshold;
-    double trackWidth;
+   // Constructor initializes robot state
+   double goalX;
+   double goalY;
+   double goalThreshold;
+   double trackWidth;
 
-    double maxLinearVelocity;
-    double maxAngularVelocity;
+   double maxLinearVelocity;
+   double maxAngularVelocity;
 
-    double K_position;
-    double K_orientation;
+   double K_position;
+   double K_orientation;
 
-    PositionControl( double goalX, double goalY, double goalThreshold, double trackWidth,
-     double maxLinearVelocity, double maxAngularVelocity, double K_position, double K_orientation) 
-     : goalX(goalX)
-     , goalY(goalY)
-     , goalThreshold(goalThreshold)
-     , trackWidth(trackWidth)
-     , maxLinearVelocity(maxLinearVelocity)
-     , maxAngularVelocity(maxAngularVelocity)
-     , K_position(K_position)
-     , K_orientation(K_orientation)
-     {}
+   IntervalTimer updateTimer;
+
+   PositionControl( double goalX, double goalY, double goalThreshold, double trackWidth,
+    double maxLinearVelocity, double maxAngularVelocity, double K_position, double K_orientation, unsigned long interval)
+    : goalX(goalX)
+    , goalY(goalY)
+    , goalThreshold(goalThreshold)
+    , trackWidth(trackWidth)
+    , maxLinearVelocity(maxLinearVelocity)
+    , maxAngularVelocity(maxAngularVelocity)
+    , K_position(K_position)
+    , K_orientation(K_orientation)
+    , updateTimer(interval)
+    {}
+
+   std::pair<double, double> position_control(Pose pose, Pose GOAL)
+   {
+       double d = distance(pose, GOAL);
+       double v_left, v_right = 0;
+
+       if (d < goalThreshold)
+       {
+           return std::make_pair(0, 0);
+       }
+
+       double v = K_position * d;
+       v = std::min(v, maxLinearVelocity);
+
+       double angle_to_goal = angle(pose, GOAL);
+       double theta_error = angle_to_goal - pose.theta;
+       double theta_dot = K_orientation * theta_error;
+       theta_dot = std::min(theta_dot, maxAngularVelocity);
+
+       v_left = v - theta_dot * trackWidth / 2.0;
+       v_right = v + theta_dot * trackWidth / 2.0;
+
+       return std::make_pair(v_left, v_right);
+   }
+
+   double distance(Pose pose, Pose GOAL)
+   {
+       return sqrt(pow(GOAL.x - pose.x, 2) + pow(GOAL.y - pose.y, 2));
+   }
 
 
+   double angle(Pose pose, Pose GOAL)
+   {
+      return atan2(GOAL.y - pose.y, GOAL.x - pose.x);
+   }
 
-    double position_control(Pose pose, Pose GOAL)
-    {
-        double d = distance(pose, GOAL);
+   void setup() {}
 
-        double v_left, v_right = 0;
+   // Loop step returns a bool but as a side effect, also sets velocities using position control function
+   bool loopStep(Pose pose, float& leftVelocity, float& rightVelocity){
+       if (!updateTimer) {
+           return false;
+       }
+       // target pose
+       Pose targetPose = Pose(goalX, goalY, K_orientation);
 
-        if (d < 0.1)
-        {
-            return v_left, v_right;
-        }
+       std::pair<double, double> velocities = position_control(pose, targetPose);
+       leftVelocity = velocities.first;
+       rightVelocity = velocities.second;
 
-        double v = K_position * d;
-        v = std::min(v, maxLinearVelocity);
-
-        double angle_to_goal = angle(pose, GOAL);
-        double theta_error = angle_to_goal - pose.theta;
-        double theta_dot = K_orientation * theta_error;
-        theta_dot = std::min(theta_dot, maxAngularVelocity);
-
-        v_left = v - theta_dot * trackWidth / 2;
-        v_right = v + theta_dot * trackWidth / 2;
-
-        return v_left, v_right;
-    }
-
-    double distance(Pose pose, Pose GOAL)
-    {
-        return sqrt(pow(GOAL.x - pose.x, 2) + pow(GOAL.y - pose.y, 2));
-    }
-
-    double angle(Pose pose, Pose GOAL)
-    {
-       return atan2(GOAL.y - pose.y, GOAL.x - pose.x);
-    }
-
-    void setup()
-    {
-    }
-
-    // Loop step function (placeholder for future use)
-    void loopStep()
-    {
-    }
+       return true;
+       }
 };
-
 
 #endif // POSITIONCONTROL_H
