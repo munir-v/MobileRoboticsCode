4bf83477a3d7a27c0b288935f67814e4107c396d
Kerick Walker <kerickwalker@Kericks-MacBook-Pro-7.local>
2024-11-18 23:32:23 -0800

    remove duplicate simulation

--- pose-simulation/forwardkinematics.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def update_position(pose, v_left, v_right, dt, track_width):
-    v = (v_left + v_right) / 2
-    theta_dot = (v_right - v_left) / track_width
-
-    pose.x += v * math.cos(pose.theta) * dt
-    pose.y += v * math.sin(pose.theta) * dt
-    pose.theta += theta_dot * dt
